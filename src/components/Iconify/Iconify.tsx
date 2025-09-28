f00c562b565670baeb5477d5df4de1f0126e4b5e
浩丶IMOYH <oyhemail@163.com>
2025-09-28 22:24:10 +0800

    feat(Iconify): 添加自定义太阳图标集合支持
    - 从本地solar.json文件导入自定义太阳图标集合
    - 使用addCollection方法将太阳图标集合注册到Iconify库
    - 确保Icon组件能够识别并渲染自定义太阳图标集中的图标

--- src/components/Iconify/Iconify.tsx
+++ src/components/Iconify/Iconify.tsx
@@ -1,5 +1,8 @@
-import { Icon, IconProps } from '@iconify/react';
+import { Icon, IconProps, addCollection } from '@iconify/react';
 import React from 'react';
+
+import solarIcons from './icons/solar.json';
+addCollection(solarIcons);
 
 export const Iconify: React.FC<IconProps> = (props) => {
   return <Icon {...props} />;
